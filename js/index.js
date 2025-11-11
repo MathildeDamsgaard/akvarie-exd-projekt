ce4720761966fbaade6db000764fb970557d6baa
Cecilie0306 <eaa25cand@students.eaaa.dk>
2025-11-11 18:53:23 +0100

    js-ændringer

--- js/index.js
+++ js/index.js
@@ -1,8 +1,65 @@
 "use strict";
+// Opbygger vores datastruktur med et Array som indeholder JS objekter
+const fishInfo = [
+    {
+        className: "fish1",
+        fishName: "Crab",
+        lifeSpan: "3-12 year",
+    },
+    {
+        className: "fish2",
+        fishName: "Nemo",
+        lifeSpan: "6-10 year",
+    },
+    {
+        className: "fish3",
+        fishName: "Pufferfish",
+        lifeSpan: "10 year",
+    },
+    {
+        className: "fish4",
+        fishName: "Shark",
+        lifeSpan: "70-80 year",
+    },
+    {
+        className: "fish5",
+        fishName: "Stinggray",
+        lifeSpan: "15-25 year",
+    },    
+    {
+        className: "fish6",
+        fishName: "Turtle",
+        lifeSpan: "20-50 year",
+    }
+];
+
+
+
 // Info kort om fiskene
 const tooltip = document.getElementById("tooltip");
 
+function showTooltip(html) {
+    if (tooltip) {
+        tooltip.innerHTML;
+        tooltip.classList.add("is-visible");
+        setTimeout( function () {
+            tooltip.classList.remove("is-visible");
+        },8000);
+    }
+}
 
+
+fishInfo.forEach((fish) => {
+    document.querySelectorAll("." + fish.className).forEach((elem) => {
+        elemaddEventListener("click", () => {
+            const fishDetails = `
+            <strong>${fish.fishName}</strong><br>
+            Year: ${fish.lifeSpan}
+            `;
+            showTooltip(fishDetails);
+        });
+    });
+});
 
 
 
